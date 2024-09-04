2c7ed5075eefac5ce39279bf0df395ef2bd1121c
Nike Chan <nike.chan@101medialab.com>
2024-09-04 18:54:13 +0800

    build

--- dist/components/ImageItem/ImageItem.android.js
+++ dist/components/ImageItem/ImageItem.android.js
@@ -81,12 +81,23 @@ const ImageItem = ({ imageSrc, onZoom, onRequestClose, onLongPress, delayLongPre
     })}>
       <View style={{ height: SCREEN_HEIGHT }}/>
       <Animated.View {...panHandlers} style={imageStylesWithOpacity}>
+        <View style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: -1,
+        }}>
+            {(isLoaded || imageDimensions) && <ImageLoading />}
+        </View>
         <ExpoImage source={imageSrc} style={{
             width: "100%",
             height: "100%",
         }} onLoad={onLoaded}/>
       </Animated.View>
-      {(!isLoaded || !imageDimensions) && <ImageLoading />}
     </ScrollView>);
 };
 const styles = StyleSheet.create({
